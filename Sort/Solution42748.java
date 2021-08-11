1d0f5af306c00aa060819fcb5afff5dc663157b5
HyeonJu-Yoo <70135303+HyeonJu-Yoo@users.noreply.github.com>
2021-08-11 18:36:24 +0900

    Create Solution42748.java
    프로그래머스 K번째 수 (문제번호 42748)

---
+++ Sort/Solution42748.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution42748 {
+    // K번째 수
+
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[(commands[i][2] - 1)];
+        }
+
+        return answer;
+    }
+}
