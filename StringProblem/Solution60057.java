5e7fe44f99562672a0bc2641faa091a12e33cc64
HyeonJu-Yoo <70135303+HyeonJu-Yoo@users.noreply.github.com>
2021-08-22 13:51:55 +0900

    60057(문자열 압축) - stack을 활용함. - string buffer에 마지막 부분을 처리하는 방법 개선 필요. - math.min을 사용하면 3항연산자보다 간결함.

--- StringProblem/Solution60057.java
+++ StringProblem/Solution60057.java
@@ -1,66 +1,49 @@
-
 import java.util.*;
 import java.lang.Math;
 
-public class Solution60057 {
-    public static int solution(String s) {
-        // 1.문자 길이 만큼 잘라서 압축해보고 가장 짧은 것을 기억한다.
-        // 2.처음 글자가 시작되는 위치를 먼저 확인해봄.
-        // -> 근데 처음글자만 짧을수도 있음.
-        int answer = 0;
-        int l = s.length();
-        int min = l, cnt = 0;
+class Solution60057 {
+    Stack<String> stack = new Stack<>();
+
+    public int solution(String s) {
+        int answer = s.length();
         String sub = "";
-        // version 1
-        // 길이만큼.
-        for (int i = 1; i <= l; i++) {
-            StringBuffer sb = new StringBuffer(); // 압축 결과
+        for (int i = 1; i <= s.length(); i++) {
             String cur = s;
-            cnt = 0;
+            int cnt = 1;
+            StringBuffer sb = new StringBuffer();
+            stack.clear();
+
             while (!cur.equals("")) {
-                // 문자열을 잘라서 비교
+                // 문자열 자르기
                 if (cur.length() < i) {
                     sub = cur.substring(0, cur.length());
                 } else {
                     sub = cur.substring(0, i);
                 }
-                // cur = cur.substring(sub.length(), cur.length());
-                while (!cur.equals("")) {
-                    System.out.println("sub : " + sub);
-                    System.out.println("cur : " + cur);
-
-                    // sub와 같으면 cnt+=1;
-                    if (cur.substring(0, sub.length()).equals(sub)) {
-                        cur = cur.substring(sub.length(), cur.length());
+                cur = cur.substring(sub.length(), cur.length());
+                // 기존 값과 비교
+                if (!stack.isEmpty()) {
+                    String temp = stack.peek();
+                    // 문자열 같으면 +1
+                    if (temp.equals(sub)) {
                         cnt += 1;
-                        if (cur.equals("")) {
-                            if (cnt != 1)
-                                sb.append(cnt);
-                            sb.append(sub);
-                            System.out.println("sb : " + sb);
-                        }
                     } else {
-                        // 다르면 break;
                         if (cnt != 1)
                             sb.append(cnt);
-                        sb.append(sub);
-                        cnt = 0;
-                        System.out.println("sb : " + sb);
-                        break;
+                        sb.append(temp);
+                        stack.add(sub);
+                        cnt = 1;
                     }
-
+                } else {
+                    stack.add(sub);
                 }
             }
-            min = Math.min(sb.length(), min);
+            if (cnt != 1)
+                sb.append(cnt);
+            sb.append(sub);
+            answer = Math.min(sb.length(), answer);
         }
-
         return answer;
     }
 
-    public static void main(String[] args) {
-
-        Solution60057.solution("aabbaccc");
-    }
 }
-
-// 2a2ba3c
